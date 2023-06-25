2246015c9750503d8bdb371ac1669804b09e1d4c
Gabriel <gabriel19gnp@gmail.com>
2023-06-25 19:33:57 -0300

    #

---
+++ Aula 16 desafio 72.py
@@ -0,0 +1,8 @@
+nex = ("um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezeseis", "dezesete", "dezoito", "dezenove", "vinte")
+
+escolha = int(input("Digite um número entre 0 e 20: "))
+while escolha > 20 or escolha < 0:
+    escolha = int(input("\033[1;31;40mNumero inválido\033[m digite um número entre 0 e 20: "))
+print(f"Você digitou o número {nex[(escolha - 1)]}.")
+
+    # Tudo Certo!
