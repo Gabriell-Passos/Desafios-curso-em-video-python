5f1a5bf7cafa81201126bd62637167c63c5d4ac9
Gabriel <gabriel19gnp@gmail.com>
2023-12-30 16:30:53 -0300

    #

--- Aula 17 desafio 78.py
+++ Aula 17 desafio 78.py
@@ -1,4 +1,8 @@
 lista = []
 for x in range (1,6):
-    e = lista.append(int(input(f"Digite o {x}° número da lista: ")))
-print(lista)
+    e = lista.append(input(f"Digite o {x}° número da lista: "))
+mn = max(lista)
+mm = min(lista)
+print(f"Na lista {lista} criada por você, o maior número é {mn} que está na posição {lista.index(mn) + 1} e o menor número é {mm} que está na posição {lista.index(mm) + 1}.")
+lista.sort()
+print(f"A lista com os números em ordem crescente fica {lista}")
