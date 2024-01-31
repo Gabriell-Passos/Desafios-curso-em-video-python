0a4841828c53ac65bd12ea3d608653357c39da2d
Gabriel <gabriel19gnp@gmail.com>
2024-01-31 13:15:20 -0300

    #

--- Aula 17 desafio 84.py
+++ Aula 17 desafio 84.py
@@ -1,3 +1,13 @@
-expressao = []
-expressao.append(input("Digite a expressão matemática: "))
-print(expressao)
+pessoas = [[], []]
+inf = [[], []]
+v = int(input("Quantas pessoas serão adicionadas na lista: "))
+c = 0
+while True:
+    if v != c:
+        inf[0].append(str(input(f"Digite o nome da {c+1}° pessoa: ")))
+        inf[1].append(int(input(f"Digite o peso da {c+1}° pessoa: ")))
+        pessoas[c].append(inf)
+        c += 1
+    else:
+        break
+print(pessoas)
